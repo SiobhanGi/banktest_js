a0bf1bedee41fb40e94e0ca629bf059fe3201d1b
SiobhanGi <siobhan.goggins@gmail.com>
2018-06-18 14:35:49 +0100

    added simple-mock to dependecies

--- test/account-test.js
+++ test/account-test.js
@@ -1,5 +1,6 @@
 let Account = require('../src/account');
-let expect = require('chai').expect
+let expect = require('chai').expect;
+let simple = require('simple-mock');
 
 describe('Account', function() {
   var fakeAccount;
@@ -13,4 +14,10 @@ describe('Account', function() {
       expect(fakeAccount._balance).to.equal(0);
     });
   });
+
+  describe('showBalance', function() {
+    it('shows the current balance of the account', function() {
+      expect(fakeAccount.showBalance()).to.equal(0);
+    })
+  })
 })
